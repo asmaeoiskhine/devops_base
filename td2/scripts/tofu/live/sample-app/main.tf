e61f7b535c7ad683f24d3547cdca6d5d20adbfd8
Oiskhine <asmae.oiskhine@edu.esiee.fr>
2026-01-16 18:25:32 +0100

    maj

--- td2/scripts/tofu/live/sample-app/main.tf
+++ td2/scripts/tofu/live/sample-app/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
 region = "us-east-1" 
 } 
 
-module "sample_app" {
-source = "github.com/asmaeoiskhine/devops.git//modules/ec2-instance" 
+module "sample_app_1" {
+source = "github.com/asmaeoiskhine/devops.git//td2/scripts/modules/ec2-instance" 
 ami_id = "ami-0b306fbb0d2a216ed"  # Replace with your AMI ID 
 name   = "sample-app-from-github"
 instance_type = "t3.micro"
