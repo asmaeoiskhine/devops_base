8439fa3e69f50a0eb920b9a3933eb6a3af818a39
Oiskhine <asmae.oiskhine@edu.esiee.fr>
2026-01-17 15:52:23 +0100

    suite lab3

--- td3/scripts/tofu/live/eks-sample/main.tf
+++ td3/scripts/tofu/live/eks-sample/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 module "cluster" {
-  source = "github.com/brikis98/devops-book//ch3/tofu/modules/eks-cluster"
+  source = "github.com/asmaeoiskhine/devops_base//ch3/tofu/modules/eks-cluster"
 
   name        = "eks-sample"        
   eks_version = "1.29"              
 
-  instance_type        = "t2.micro" 
+  instance_type        = "t3.micro" 
   min_worker_nodes     = 1          
   max_worker_nodes     = 10         
   desired_worker_nodes = 3          
